68f1705408fd7e6205e99dab5679c1d1502def17
Кристина Курец <honey_kris@outlook.com>
2020-10-04 10:59:58 +0300

    stored procedures added into migration

---
+++ Data/Migrations/20201002191450_RunSqlScript.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.IO;
+
+namespace Data.Migrations
+{
+    public partial class RunSqlScript : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var overspeed = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"OverSpeed.sql");
+            File.ReadAllText(overspeed);
+
+            var minmaxspeed = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"MinMaxSpeed.sql");
+            File.ReadAllText(minmaxspeed);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
